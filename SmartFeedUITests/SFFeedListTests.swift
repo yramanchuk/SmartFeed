3f8616b20263a303f31da72298c1a942cf38ce9f
YuryR <yramanchuk@users.noreply.github.com>
2016-04-07 19:39:00 +0300

    add initial unit tests

---
+++ SmartFeedUITests/SFFeedListTests.swift
@@ -0,0 +1,13 @@
+//
+//  SFFeedListTests.swift
+//  SmartFeed
+//
+//  Created by Yury Ramanchuk on 4/7/16.
+//  Copyright © 2016 Yury Ramanchuk. All rights reserved.
+//
+
+import Cocoa
+
+class SFFeedListTests: KIFTestCase {
+
+}
